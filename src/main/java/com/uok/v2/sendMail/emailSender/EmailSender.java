7b321e0a1d804964c51232d06154b2af3fb5adb5
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-10-02 11:14:19 +0530

    add GmailSMTP

---
+++ src/main/java/com/uok/v2/sendMail/emailSender/EmailSender.java
@@ -0,0 +1,4 @@
+package com.uok.v2.sendMail.emailSender;
+
+public interface EmailSender {
+}
---
+++ src/main/java/com/uok/v2/sendMail/emailSender/gmail/GmailSMTP.java
@@ -0,0 +1,10 @@
+package com.uok.v2.sendMail.emailSender.gmail;
+
+public class GmailSMTP {
+    public void sendEmail(String FilePath, String filePath){
+        final String sender = "thusharadasun204@gmail.com";
+        final String password = "";
+
+
+    }
+}
