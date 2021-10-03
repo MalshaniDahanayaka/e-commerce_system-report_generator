1ba7f0f6eb0bead611d334355d7a62d3e169f71c
Radhushani Samundeeshwaran <62767392+radhushani@users.noreply.github.com>
2021-10-03 14:58:06 +0530

    Operations-final code (#10)

---
+++ src/main/java/com/uok/final_version/operations/InvalidReportSenderAppOperationException.java
@@ -0,0 +1,7 @@
+package com.uok.final_version.operations;
+
+public class InvalidReportSenderAppOperationException extends Throwable{
+    public InvalidReportSenderAppOperationException(String message) {
+        super(message);
+    }
+}
